04a62d9f3e8a9041f9a78dced68c20ddc87073f0
Michal Cwiekala <michal.cwiekala-extern@dbschenker.com>
2022-08-08 08:21:24 +0200

    lekcja 1

---
+++ src/lekcja1/metoda/main/folder1/Runner.java
@@ -0,0 +1,26 @@
+package lekcja1.metoda.main.folder1;
+
+public class Runner {
+
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Metoda main");
+        test1();
+        Runner.test1();
+        test2();
+        test3();
+    }
+
+    public static void test1() {
+        System.out.println("test 1");
+    }
+
+    public static void test2() {
+        System.out.println("test 2");
+    }
+
+    static void test3() {
+
+    }
+}
---
+++ src/lekcja1/metoda/main/folder2/Runner.java
@@ -0,0 +1,8 @@
+package lekcja1.metoda.main.folder2;
+
+class Runner {
+
+    public static void main(String[] args) {
+        System.out.println("lekcja1.metoda.main.folder1.Runner 2");
+    }
+}
