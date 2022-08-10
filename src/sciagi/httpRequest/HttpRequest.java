d237774bcb511a9a7f99714b4d607f5366a4cedf
Adrian Goral <adrian.goral@gmail.com>
2022-08-10 15:11:51 +0200

    Update HttpRequest.java

--- src/sciagi/httpRequest/HttpRequest.java
+++ src/sciagi/httpRequest/HttpRequest.java
@@ -14,6 +14,36 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
+    
+    /**
+     * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+     * <p>
+     * Copyright © 2017, Calculistik . All rights reserved.
+     * <p>
+     * Oracle and Java are registered trademarks of Oracle and/or its
+     * affiliates. Other names may be trademarks of their respective owners.
+     * <p>
+     * The contents of this file are subject to the terms of either the GNU
+     * General Public License Version 2 only ("GPL") or the Common
+     * Development and Distribution License("CDDL") (collectively, the
+     * "License"). You may not use this file except in compliance with the
+     * License. You can obtain a copy of the License at
+     * https://netbeans.org/cddl-gplv2.html or
+     * nbbuild/licenses/CDDL-GPL-2-CP. See the License for the specific
+     * language governing permissions and limitations under the License.
+     * When distributing the software, include this License Header
+     * Notice in each file and include the License file at
+     * nbbuild/licenses/CDDL-GPL-2-CP. Oracle designates this particular file
+     * as subject to the "Classpath" exception as provided by Oracle in the
+     * GPL Version 2 section of the License file that accompanied this code. If
+     * applicable, add the following below the License Header, with the fields
+     * enclosed by brackets [] replaced by your own identifying information:
+     * "Portions Copyrighted [year] [name of copyright owner]"
+     * <p>
+     * Contributor(s):
+     * Created by alejandro tkachuk @aletkachuk
+     * www.calculistik.com
+     */
 
     public static enum Method {
         POST, PUT, DELETE, GET;
